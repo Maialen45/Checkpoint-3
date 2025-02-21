839c1a754599af5ff98ae4aff57acf73ff86887c
Maialen45 <maialenes@gmail.com>
2025-02-21 09:55:09 +0100

    checkpoint 3

---
+++ M2C3.py
@@ -0,0 +1,46 @@
+#Exercise 1:
+string = 'Hello world'
+number = 4
+list = ['string', 'number', 'list', 'boolean']
+boolean = True
+
+#Exercise 2
+first_three_letters = string[0:3]
+print(first_three_letters)
+
+#Exercise 3
+first_element = list[0]
+print(first_element)
+
+#Exercise 4
+new_number = number + 10
+print(new_number)
+
+#Exercise 5
+last_element = list[-1]
+print(last_element)
+
+#Exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+list_names = names.split(',')
+print(list_names)
+
+#Exercise 7
+first_word = string[0:5].upper()
+print(first_word)
+new_string = first_word + string[5:]
+print(new_string)
+
+#Exercise 8
+sentence = f'Hi, I am {number} years old'
+print(sentence)
+
+#Exercise 9
+print('hello world')
+
+#Extra exercise
+cadena = 'Hola mundo'
+query = cadena.find('Hola')
+print(query)
+nueva_cadena = cadena.replace('Hola','adiós')
+print(nueva_cadena)
